e5ba0967f8f3810eb238965176105c7b3c73e34b
Cendy <cendyemail@gmail.com>
2023-03-08 00:35:23 +0700

    feat/intialize electron

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <>
+    <h1 className="text-rose-500">Hello world</h1>
+  </>
+);
